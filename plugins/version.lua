2136b2b2449f8b49acf4593e867d26f3e8e9d99e
cloner-hooshesiyah <loggerrin@yahoo.com>
2016-04-20 14:32:43 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,21 +1,21 @@
 
 --------------------------------------------------
 --                                              --
---       Developers: @heset_ni_zendgi @Xx_admin_eblis_xX      --
---     our chanal:  @eblis_unity   --
+--       Developers: @SUDO_USER     --
+--     our chanal:  @NoosSpeedbot   --
 --                                              --
 --------------------------------------------------
 
 do
 
 function run(msg, matches)
-  return '❄️Freeze robot V1❄️\n                                                                        💥created by Eblis team💥\n\n 🏅 sudo users 🏅                                                                                                                                                         @heset_ni_zendgi                                               @Xx_admin_eblis_xX                                                                             ️\n ➡️ our chanal: @eblis_unity⬅                                                       ️〰〰〰〰〰〰〰〰〰〰〰'
+  return '❄️Unix Bot V1❄️\n                                                                        💥created by Eblis team💥\n\n 🏅 sudo users 🏅                                                                                                                                                         @heset_ni_zendgi                                               @Xx_admin_eblis_xX                                                                             ️\n ➡️ our chanal: @eblis_unity⬅                                                       ️〰〰〰〰〰〰〰〰〰〰〰'
   
 end
 
 return {
   patterns = {
-    "^#version$"
+    "^#Unixbot$"
   }, 
   run = run 
 }
